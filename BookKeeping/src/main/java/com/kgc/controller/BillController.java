7033a5d77d277f584d6015a8169ed209054ab908
changxin <1025036729@qq.com>
2020-10-01 14:47:24 +0800

    记账

---
+++ BookKeeping/src/main/java/com/kgc/controller/BillController.java
@@ -0,0 +1,41 @@
+package com.kgc.controller;
+
+import com.kgc.pojo.Bills;
+import com.kgc.pojo.Billtype;
+import com.kgc.service.BillService;
+import com.kgc.service.BillTypeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Controller
+public class BillController {
+    @Resource
+    BillTypeService billTypeService;
+    @Resource
+    BillService billService;
+
+    @RequestMapping("/selectAll")
+    public String selectAll(Model model,Integer typeId){
+        List<Bills> bills = billService.selectAll();
+        List<Billtype> billtypes = billTypeService.selectAll();
+        model.addAttribute("billtypes",billtypes);
+        model.addAttribute("bills",bills);
+        return "index";
+    }
+
+    @RequestMapping("/add")
+    public String add(Bills bills,Integer typeid,String billtime,Integer price,String explains,String title){
+//        bills.setTitle(title);
+//        bills.setBilltime(billtime);
+//        bills.setPrice(price);
+//        bills.setExplains(explains);
+//        bills.setTypeid(typeid);
+        billService.add(bills);
+        return "redirect:selectAll";
+    }
+}
+
